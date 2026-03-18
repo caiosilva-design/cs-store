dc48419fce1fcf7f56ed78aef7ea4ad360fbb407
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-18 12:48:22 -0300

    Add files via upload

---
+++ app/components/WhatsappButton.tsx
@@ -0,0 +1,21 @@
+export default function WhatsappButton() {
+ return (
+<a
+     href="https://wa.me/5511972734037"
+     target="_blank"
+     style={{
+       position: "fixed",
+       bottom: "20px",
+       right: "20px",
+       background: "#25D366",
+       color: "white",
+       padding: "15px",
+       borderRadius: "50%",
+       fontSize: "20px",
+       textDecoration: "none"
+     }}
+>
+     💬
+</a>
+ );
+}
