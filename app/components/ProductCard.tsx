cb0dd2d8c9830104b91fe2a92bc79ce8add4df9d
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-18 12:14:15 -0300

    Update ProductCard.tsx

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,17 +1,27 @@
+"use client";
+export default function ProductCard({ produto }: any) {
+ return (
+<div style={{ padding: "10px", border: "1px solid #eee" }}>
+<img src={produto.imagem} style={{ width: "100%" }} />
+<h3>{produto.nome}</h3>
+     {/* PREÇO */}
 <div style={{ marginTop: "5px" }}>
- {produto.preco_antigo && (
+       {produto.preco_antigo && (
 <span
-     style={{
-       textDecoration: "line-through",
-       color: "#999",
-       marginRight: "8px",
-       fontSize: "14px",
-     }}
+           style={{
+             textDecoration: "line-through",
+             color: "#999",
+             marginRight: "8px",
+             fontSize: "14px",
+           }}
 >
-     R$ {produto.preco_antigo}
+           R$ {produto.preco_antigo}
 </span>
- )}
+       )}
 <span style={{ fontWeight: "bold", color: "#000" }}>
-   R$ {produto.preco}
+         R$ {produto.preco}
 </span>
 </div>
+</div>
+ );
+}
