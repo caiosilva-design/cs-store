94cbd720e8b609350a249fbbf4c884ec66442797
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-18 11:47:27 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "./components/ProductCard";
 async function getProdutos() {
  const res = await fetch("https://cs-store-api-production.up.railway.app/produtos", {
    cache: "no-store",
@@ -7,10 +8,8 @@ async function getProdutos() {
 export default async function Home() {
  const produtos = await getProdutos();
  return (
-<div style={{ padding: "40px", fontFamily: "Arial" }}>
-<h1 style={{ fontSize: "32px", marginBottom: "30px" }}>
-       🔥 CS STORE
-</h1>
+<div style={{ padding: "40px" }}>
+<h1>🔥 CS STORE</h1>
 <div
        style={{
          display: "grid",
@@ -19,66 +18,7 @@ export default async function Home() {
        }}
 >
        {produtos.map((p: any) => (
-<div
-           key={p.id}
-           style={{
-             border: "1px solid #eee",
-             borderRadius: "10px",
-             padding: "15px",
-             boxShadow: "0 2px 8px rgba(0,0,0,0.05)",
-           }}
->
-           {/* IMAGEM */}
-<img
-             src={p.imagem}
-             alt={p.nome}
-             style={{
-               width: "100%",
-               height: "200px",
-               objectFit: "cover",
-               borderRadius: "8px",
-             }}
-           />
-           {/* NOME */}
-<h2 style={{ fontSize: "16px", marginTop: "10px" }}>
-             {p.nome}
-</h2>
-           {/* PREÇO */}
-<p style={{ fontWeight: "bold", margin: "5px 0" }}>
-             💰 R$ {p.preco}
-</p>
-           {/* TAMANHOS */}
-<div>
-<strong style={{ fontSize: "14px" }}>
-               Tamanhos:
-</strong>
-<div
-               style={{
-                 display: "flex",
-                 gap: "8px",
-                 marginTop: "5px",
-                 flexWrap: "wrap",
-               }}
->
-               {p.variacoes?.map((v: any, i: number) => (
-<span
-                   key={i}
-                   style={{
-                     padding: "5px 10px",
-                     borderRadius: "5px",
-                     fontSize: "12px",
-                     border: "1px solid",
-                     backgroundColor: v.disponivel ? "#fff" : "#eee",
-                     color: v.disponivel ? "#000" : "#999",
-                     textDecoration: v.disponivel ? "none" : "line-through",
-                   }}
->
-                   {v.tamanho}
-</span>
-               ))}
-</div>
-</div>
-</div>
+<ProductCard key={p.id} produto={p} />
        ))}
 </div>
 </div>
