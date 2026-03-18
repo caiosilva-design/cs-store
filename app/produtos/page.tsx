b9300bd90d2c87a28b3370d44dbcdbb6102f1231
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-18 11:52:14 -0300

    Add files via upload

---
+++ app/produtos/page.tsx
@@ -0,0 +1,20 @@
+import ProductCard from "../components/ProductCard";
+async function getProdutos() {
+ const res = await fetch("https://SUA_API/produtos", {
+   cache: "no-store",
+ });
+ return res.json();
+}
+export default async function Produtos() {
+ const produtos = await getProdutos();
+ return (
+<div style={{ padding: "40px" }}>
+<h1>Produtos</h1>
+<div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
+       {produtos.map((p: any) => (
+<ProductCard key={p.id} produto={p} />
+       ))}
+</div>
+</div>
+ );
+}
