c379092424e2e979b95d7d1582454e6a97391f25
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-19 10:05:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,25 @@
-import Navbar from "./components/Navbar";
-import WhatsappButton from "./components/WhatsappButton";
+import "./globals.css";
+import Footer from "./components/Footer";
+export const metadata = {
+ title: "CRIA STUDIO",
+ description: "Camisas premium e personalizadas"
+};
 export default function RootLayout({ children }: any) {
  return (
-<html lang="pt-BR">
-<body>
-<Navbar />
-       {children}
-<WhatsappButton />
+<html lang="pt-br">
+<body
+       style={{
+         display: "flex",
+         flexDirection: "column",
+         minHeight: "100vh"
+       }}
+>
+       {/* CONTEÚDO */}
+<main style={{ flex: 1 }}>
+         {children}
+</main>
+       {/* RODAPÉ GLOBAL */}
+<Footer />
 </body>
 </html>
  );
