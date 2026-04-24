b11f8529ad920e2e026715676fabd3b2e12ae9c7
caiosilva-design <caiodasilvacaio@gmail.com>
2026-04-24 10:31:02 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
- images: {
-   domains: ["i0.wp.com", "playmodrop.com.br"],
- },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
